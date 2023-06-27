abfdf0a9dd75c0e058616127cc2ea12d764746bf
Simon Gamble <simon.gamble@avanade.com>
2023-06-27 16:25:10 +0930

    Initial Commit

---
+++ NavigationStack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by Simon Gamble on 27/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
