94bb39a475fa2dbbfc3f17e8a1ea08e40f35de57
gamble2020 <75001472+gamble2020@users.noreply.github.com>
2023-06-27 18:28:17 +0930

    Update Page3.swift

--- NavigationStack/Page3.swift
+++ NavigationStack/Page3.swift
@@ -1,20 +1,29 @@
-//
-//  Page3.swift
-//  NavigationStack
-//
-//  Created by Simon Gamble on 27/6/2023.
-//
-
 import SwiftUI
 
 struct Page3: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct Page3_Previews: PreviewProvider {
-    static var previews: some View {
-        Page3()
+    var message: String
+
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(message)
+            Spacer()
+            Button(
+                action: {
+                    NavigationService.popView()
+                })
+            {
+                Text("Pop to page 2")
+            }
+            Button(
+                action: {
+                    NavigationService.popToRoot()
+                })
+            {
+                Text("Pop to root")
+            }
+        }
+        .padding(20)
+        .navigationTitle("Page 3")
     }
 }
