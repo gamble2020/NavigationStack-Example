ae8849276a8101c9f1392890658016d6fa58065d
gamble2020 <75001472+gamble2020@users.noreply.github.com>
2023-06-27 18:27:32 +0930

    Update NavigationService.swift

--- NavigationStack/NavigationService.swift
+++ NavigationStack/NavigationService.swift
@@ -5,4 +5,41 @@
 //  Created by Simon Gamble on 27/6/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct NavigationService {
+
+    static func pushView(content: AnyView) {
+        let vc = UIHostingController(rootView: content)
+        findRootViewController()?.pushViewController(vc, animated: true)
+    }
+
+    static func popView() {
+        findRootViewController()?.popViewController(animated: true)
+    }
+
+    static func popToRoot() {
+        findRootViewController()?.popToRootViewController(animated: true)
+    }
+    
+    static func findRootViewController() -> UINavigationController? {
+        return findNavigationController(viewController: UIApplication
+            .shared
+            .connectedScenes
+            .compactMap { ($0 as? UIWindowScene)?.keyWindow }
+            .first?.rootViewController)
+    }
+
+    static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
+        guard let viewController = viewController else {
+            return nil
+        }
+        if let navigationController = viewController as? UINavigationController {
+            return navigationController
+        }
+        for childViewController in viewController.children {
+            return findNavigationController(viewController: childViewController)
+        }
+        return nil
+    }
+}
