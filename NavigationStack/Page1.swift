a4561731119480ac3f3534c17c799c3834398c26
gamble2020 <75001472+gamble2020@users.noreply.github.com>
2023-06-27 18:28:32 +0930

    Update Page1.swift

--- NavigationStack/Page1.swift
+++ NavigationStack/Page1.swift
@@ -1,26 +1,19 @@
-//
-//  ContentView.swift
-//  NavigationStack
-//
-//  Created by Simon Gamble on 27/6/2023.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct Page1: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("Hi there, I'm page 1")
+            Spacer()
+            Button(
+                action: {
+                    NavigationService.pushView(content: AnyView(Page2(message: "Hi there, I'm page 2")))
+                })
+            {
+                Text("Push to page 2")
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        .padding(20)
+        .navigationTitle("Page 1")
     }
 }
